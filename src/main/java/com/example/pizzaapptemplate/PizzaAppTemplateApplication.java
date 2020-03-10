b2bd205a1e778f83359d99510a24488e2e18f2e5
Matthew <matthew.robert.leroe@gmail.com>
2020-03-10 10:22:18 -0400

    Initial commit

---
+++ src/main/java/com/example/pizzaapptemplate/PizzaAppTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.pizzaapptemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaAppTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaAppTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pizzaapptemplate/PizzaAppTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pizzaapptemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaAppTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
